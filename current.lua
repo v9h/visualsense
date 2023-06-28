87053d929d5e604afcd529697b58350b7d4fc2c7
v9h <94395605+v9h@users.noreply.github.com>
2023-06-28 16:54:02 +0300

    Create current.lua

---
+++ current.lua
@@ -0,0 +1,199 @@
+--[[
+PREPRE-release, only contains character modification
+
+Please try to Google the way to get a TXID of an item.
+]]
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "VisualSense™",
+   LoadingTitle = "VisualSense™",
+   LoadingSubtitle = "by roll",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = 'VSFolder',
+      FileName = "VS"
+   },
+   Discord = {
+      Enabled = true,
+      Invite = "KYZ729HW4", 
+      RememberJoins = true 
+   }
+})
+local t1 = Window:CreateTab("Character Modifications") 
+local s1 = t1:CreateSection("Hats")
+local tb1 = t1:CreateInput({
+   Name = "Hat Adder",
+   PlaceholderText = "Paste Hat ID here...",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+function l(a, _)
+local b = Instance.new("Weld")
+b.Part0 = a.Parent
+b.Part1 = _.Parent
+b.C0 = a.CFrame
+b.C1 = _.CFrame
+b.Parent = a.Parent
+return b
+end
+local function b(c, b, _, a, d, e)
+local f = Instance.new("Weld")
+f.Name = c
+f.Part0 = _
+f.Part1 = a
+f.C0 = d
+f.C1 = e
+f.Parent = b
+return f
+end
+local function a(_, b)
+for _, _ in pairs(_:GetChildren()) do
+if _:IsA("Attachment") and _.Name == b then
+return _
+elseif not _:IsA("Accoutrement") and not _:IsA("Tool") then
+local _ = a(_, b)
+if _ then
+return _
+end
+end
+end
+end
+function k(_, d)
+d.Parent = _
+local e = d:FindFirstChild("Handle")
+if e then
+local c = e:FindFirstChildOfClass("Attachment")
+if c then
+local _ = a(_, c.Name)
+if _ then
+l(_, c)
+end
+else
+local c = _:FindFirstChild("Head")
+if c then
+local a = CFrame.new(0, 0, 0)
+local _ = d.AttachmentPoint
+b("HeadWeld", c, c, e, a, _)
+end
+end
+end
+end
+local _ = Text
+local _ = game:GetObjects("rbxassetid://" .. tostring(_))[1]
+k(game.Players.LocalPlayer.Character, _)
+   end,
+})
+local tb2 = t1:CreateInput({
+   Name = "Hat Adder (EFFECT)",
+   PlaceholderText = "Paste Hat ID here...",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+function l(a, _)
+local b = Instance.new("Weld")
+b.Part0 = a.Parent
+b.Part1 = _.Parent
+b.C0 = a.CFrame
+b.C1 = _.CFrame
+b.Parent = a.Parent
+return b
+end
+local function b(c, b, _, a, d, e)
+local f = Instance.new("Weld")
+f.Name = c
+f.Part0 = _
+f.Part1 = a
+f.C0 = d
+f.C1 = e
+f.Parent = b
+return f
+end
+local function a(_, b)
+for _, _ in pairs(_:GetChildren()) do
+if _:IsA("Attachment") and _.Name == b then
+return _
+elseif not _:IsA("Accoutrement") and not _:IsA("Tool") then
+local _ = a(_, b)
+if _ then
+return _
+end
+end
+end
+end
+function k(_, d)
+d.Parent = _
+local e = d:FindFirstChild("Handle")
+if e then
+local c = e:FindFirstChildOfClass("Attachment")
+if c then
+local _ = a(_, c.Name)
+if _ then
+l(_, c)
+end
+else
+local c = _:FindFirstChild("Head")
+if c then
+local a = CFrame.new(0, 0.5, 0)
+local _ = d.AttachmentPoint
+b("HeadWeld", c, c, e, a, _)
+end
+end
+end
+end
+local _ = Text
+local _ = game:GetObjects("rbxassetid://" .. tostring(_))[1]
+k(game.Players.LocalPlayer.Character, _)
+   end,
+})
+local s2 = t1:CreateSection("Clothing (TEXTUREID ONLY)")
+local tb3 = t1:CreateInput({
+   Name = "Shirt",
+   PlaceholderText = "Paste Shirt TXID here...",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = 'rbxassetid://'..Text
+   end,
+})
+local tb4 = t1:CreateInput({
+   Name = "Pants",
+   PlaceholderText = "Paste Pants TXID here...",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+game.Players.LocalPlayer.Character.Pants.PantsTemplate = 'rbxassetid://'..Text
+   end,
+})
+local s3 = t1:CreateSection("Other Body Modifications")
+local tb5 = t1:CreateInput({
+   Name = "Face (TEXTUREID ONLY)",
+   PlaceholderText = "Paste Face TXID here...",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+game.Players.LocalPlayer.Character.Head.face.Texture = 'rbxassetid://'..Text
+   end,
+})
+local tb6 = t1:CreateInput({
+   Name = "Head",
+   PlaceholderText = "Paste Head ID here...",
+   RemoveTextAfterFocusLost = true,
+   Callback = function(Text)
+game.Players.LocalPlayer.Character.Head.MeshId = 'http://roblox.com/asset/?id='..Text
+   end,
+})
+local b1 = t1:CreateButton({
+   Name = "Press for Headless",
+   Callback = function()
+game.Players.LocalPlayer.Character.Head.Transparency = 1
+game.Players.LocalPlayer.Character.Head.face:Destroy()
+game.Players.LocalPlayer.Character.Head.face:Destroy()
+   end,
+})
+local b2 = t1:CreateButton({
+   Name = "Press for Korblox LLeg",
+   Callback = function()
+game.Players.LocalPlayer.Character.LeftLowerLeg.MeshId = "http://roblox.com/asset/?id=9598310137"
+game.Players.LocalPlayer.Character.LeftLowerLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+game.Players.LocalPlayer.Character.LeftUpperLeg.MeshId = "http://www.roblox.com/asset/?id=9598310131"
+game.Players.LocalPlayer.Character.LeftUpperLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+game.Players.LocalPlayer.Character.LeftFoot.MeshId = "9598310118"
+game.Players.LocalPlayer.Character.LeftFoot.Transparency = "1"
+   end,
+})
+
